2a802941af3ea4f9f85db2831be23f4e4f1ebc19
GrimalDev <gb.titou2b@gmail.com>
2026-02-03 00:49:18 +0100

    qrcode function fish

---
+++ fish/functions/qrcode.fish
@@ -0,0 +1,11 @@
+function qrcode
+    if test (count $argv) -eq 0
+        echo "Usage: qrcode <url or text>"
+        return 1
+    end
+    set temp_file /tmp/qr_(date +%s).png
+    qrtool encode "$argv" -o $temp_file
+    osascript -e "set imageData to (read (POSIX file \"$temp_file\") as {«class PNGf»})" -e "set the clipboard to imageData"
+    rm $temp_file
+    echo "QR code for '$argv' copied to clipboard (Cmd+V to paste)"
+end
